fbda62dcd9a5eb475098b1d3b891acfe82df02c8
KristinaSig <ksigetova@uos.de>
2021-10-11 19:51:06 +0200

    implemented unit test for text cleaner

---
+++ test/preprocessing/text_cleaner_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  9 19:03:13 2021
+
+@author: KristinaSig
+"""
+
+import unittest
+import pandas as pd
+from code.preprocessing.text_cleaner import Text_cleaner
+
+class Text_CleanerTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.INPUT_COLUMN = "input"
+        self.OUTPUT_COLUMN = "output"
+        self.cleaner = Text_cleaner(self.INPUT_COLUMN, self.OUTPUT_COLUMN)
+        
+    def test_boolean(self):
+        self.assertEqual(True, not False)
+        
+    def test_input_columns(self):
+        self.assertEqual(self.cleaner._input_columns, [self.INPUT_COLUMN])
+        
+    def test_output_column(self):
+        self.assertEqual(self.cleaner._output_column, self.OUTPUT_COLUMN)
+    
+    def test_cleaning_single_tweet(self):
+        input_tweet = "$%& This is an example tweet with @preprocessor. #funwithunittests http://skgjdajkajdklja;kja;kjda"
+        output_tweet = " This is an example tweet with   "
+       
+        input_df = pd.DataFrame()
+        input_df[self.INPUT_COLUMN] = [input_tweet]
+        
+        cleaned_text = self.cleaner.fit_transform(input_df)
+        self.assertEqual(output_tweet, cleaned_text[self.OUTPUT_COLUMN][0])
+    
+    def text_cleaning_multiple_tweets(self):
+        tweet_1 = "$%& This is an example tweet with @preprocessor."
+        tweet_2 = "This is another tweet #funwithunittests http://skgjdajkajdklja;kja;kjda"
+        output_tweets = [" This is an example tweet with ", "This is another tweet  "]
+        
+        input_df = pd.DataFrame()
+        input_df[self.INPUT_COLUMN] = [tweet_1, tweet_2]
+        
+        cleaned_text = self.cleaner.fit_transform(input_df)
+        self.assertEqual(output_tweets, cleaned_text[self.OUTPUT_COLUMN][0])       
+        
+if __name__ == "__main__":
+    unittest.main()
