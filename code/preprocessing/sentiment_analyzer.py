aa590bde6a48a140420819fb90442b203bd31834
KristinaSig <ksigetova@uos.de>
2021-10-11 20:08:13 +0200

    implemented sentiment analyser
    Based on nltk's VADER. Takes (clean) text as input, outputs a score
    between -1 and 1. The closer to 1, the more positive the polarity of the
    sentiment.

---
+++ code/preprocessing/sentiment_analyzer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Preprocessor that assigns a positive, negative or neutral value to a tweet.
+
+Created on Sat Oct  9 23:33:33 2021
+
+@author: KristinaSig
+"""
+
+from code.preprocessing.preprocessor import Preprocessor
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+class Sentiment_analyzer(Preprocessor):
+    """Assigns sentiment value to input."""
+ 
+    def __init__(self, input_column, output_column):
+        super().__init__([input_column], output_column)
+ 
+    def _get_values(self, inputs):
+        
+        analyzer = SentimentIntensityAnalyzer()
+        
+        # obtain a compound score, which is a balanced version of the negative-neutral-positive scores
+        sentiment_scores = [analyzer.polarity_scores(tweet)["compound"] for tweet in inputs[0]]
+        
+        return sentiment_scores
