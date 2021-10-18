ed8424d3fd0f6a8ab878a65f58e3fadd1beafc6d
spro66 <audishantanu@gmail.com>
2021-10-18 22:00:13 +0200

    Checked SVC and Logistic Regression

--- code/classification.sh
+++ code/classification.sh
@@ -5,10 +5,10 @@ mkdir -p data/classification/
 
 # run feature extraction on training set (may need to fit extractors)
 echo "  training set"
-python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle --knn 5 -s 42 --accuracy --kappa
+python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle --logistic -s 42 --accuracy --kappa -ap
 
 # run feature extraction on validation set (with pre-fit extractors)
 echo "  validation set"
-python -m code.classification.run_classifier data/dimensionality_reduction/validation.pickle -i data/classification/classifier.pickle --accuracy --kappa
+python -m code.classification.run_classifier data/dimensionality_reduction/validation.pickle -i data/classification/classifier.pickle --accuracy --kappa -ap
 
 # don't touch the test set, yet, because that would ruin the final generalization experiment!
