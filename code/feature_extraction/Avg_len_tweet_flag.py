66d12c3454759570121346e09c1f1ebcb9ebaee1
spro66 <audishantanu@gmail.com>
2021-10-23 13:36:17 +0200

    Binary flag on the basis of avg char length

---
+++ code/feature_extraction/Avg_len_tweet_flag.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 23 13:03:53 2021
+
+@author: saudichya
+"""
+
+import numpy as np
+from code.feature_extraction.feature_extractor import FeatureExtractor
+
+# class for extracting Binary flag on the basis of Average tweet length
+class AvgLenTweet(FeatureExtractor):
+    
+    # constructor
+    def __init__(self, input_column):
+        super().__init__([input_column], "{0}_Len_Flag".format(input_column))
+    
+    # don't need to fit, so don't overwrite _set_variables()
+    
+    # compute the word length based on the inputs
+    def _get_values(self, inputs):
+        
+        result = np.array(inputs[0].str.len())
+        result = result.reshape(-1,1)
+        avg = sum(result)/len(result)
+        result1 = np.where(result > avg, 1, 0)
+        return result1
+
