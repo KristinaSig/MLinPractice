818cadafed4d7b7aadfd28398c7f9263b0011660
KristinaSig <ksigetova@uos.de>
2021-10-23 20:20:28 +0200

    mentions feature and unit test final

--- code/feature_extraction/mentions_count.py
+++ code/feature_extraction/mentions_count.py
@@ -10,10 +10,11 @@ Created on Fri Oct 15 16:33:18 2021
 
 from code.feature_extraction.feature_extractor import FeatureExtractor
 import numpy as np
+import ast
 from code.util import COLUMN_MENTIONS
 
 class MentionsCountFeature(FeatureExtractor):
-    """Creates a feature that represents the number of hashtags extracted from the tweets."""
+    """Creates a feature that represents the number of mentions extracted from the tweets."""
     
     # initialize with the input column that includes the extracted mentions
     def __init__(self):
@@ -22,8 +23,15 @@ class MentionsCountFeature(FeatureExtractor):
     
     def _get_values(self, inputs):
  
-        # exctract the number of mention ids present               
-        counts = np.array(inputs[0].str.count(pat = "'id': "))
-        counts = counts.reshape(-1,1)       
+        counts = []
+        for i in inputs[0]:
+            # first, transform each string input into a list
+            input_list = ast.literal_eval(i)
+            
+            mentions_count = len(input_list)
+            counts.append(mentions_count)
         
-        return counts
+        column = np.array(counts)
+        column = column.reshape(-1,1)     
+        
+        return column
