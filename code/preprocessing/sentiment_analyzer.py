a21cac1cb683956f55f45af0213fff6a29fb4d8f
KristinaSig <ksigetova@uos.de>
2021-10-30 14:55:48 +0200

    final check of preprocessing scripts
    included some additional comments, docstrings, corrected the order to
    fit with the other files

--- code/preprocessing/sentiment_analyzer.py
+++ code/preprocessing/sentiment_analyzer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Preprocessor that assigns a positive, negative or neutral value to a tweet.
+Preprocessor that evaluates the sentiment polarity and intensity of tweets.
 
 Created on Sat Oct  9 23:33:33 2021
 
@@ -10,19 +10,23 @@ Created on Sat Oct  9 23:33:33 2021
 
 from code.preprocessing.preprocessor import Preprocessor
 from nltk.sentiment import SentimentIntensityAnalyzer
-#from code.util import ATTR_COMPOUND
 
 class SentimentAnalyzer(Preprocessor):
-    """Assigns sentiment value to input."""
+    """Assigns sentiment values to input."""
  
     def __init__(self, input_column, output_column):
+        """Initialize the Sentiment Analyzer with the given input and output column."""
+       
         super().__init__([input_column], output_column)
+    
+    # don't need to implement _set_variables(), since no variables to set
  
     def _get_values(self, inputs):
+        """Get sentiment scores for the tweets."""
         
         analyzer = SentimentIntensityAnalyzer()
         
-        # obtain a compound score, which is a balanced version of the negative-neutral-positive scores
+        # obtain a dictionary of scores for each tweet, containing four scores (neg, neu, pos, compound)
         sentiment = [analyzer.polarity_scores(tweet) for tweet in inputs[0]]
         
         return sentiment
